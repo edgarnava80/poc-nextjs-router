31b992ecd3e308a64f133990260809b8376fe1e6
Edgar Nava <enavaramirez@coastappliances.com>
2022-07-09 11:37:32 -0700

    Changed title 2

--- next.config.js
+++ next.config.js
@@ -2,19 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // images: {
-  //   loader: 'akamai',
-  //   path: ''
-  // },
-  // basePath: '/poc-nextjs-router',
-  // assetPrefix: '/poc-nextjs-router',
-}
-
-if (process.env.NODE_ENV === 'prod') {
-  nextConfig.assetPrefix = '/poc-nextjs-router',
-    nextConfig.images.loader = 'akamai',
-    nextConfig.images.path = '',
-    nextConfig.basePath = '/poc-nextjs-router'
+  images: {
+    loader: 'akamai',
+    path: ''
+  },
+  basePath: '/poc-nextjs-router',
+  assetPrefix: '/poc-nextjs-router'
 }
 
 module.exports = nextConfig
